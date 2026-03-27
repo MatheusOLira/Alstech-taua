bd49029500d6ab20ba558e03a482d1df28e4c1f0
MatheusOLira <matheusbaska@gmail.com>
2026-03-27 16:55:11 -0300

    Avaliações concluida

--- scripts.js
+++ scripts.js
@@ -17,3 +17,65 @@ window.addEventListener('scroll', () => {
     header.style.background = "rgba(15, 23, 42, 0.7)";
   }
 });
+
+const track = document.getElementById("carouselTrack");
+const cards = document.querySelectorAll(".review");
+const dotsContainer = document.getElementById("dots");
+
+let index = 0;
+let autoSlide;
+
+// criar bolinhas
+cards.forEach((_, i) => {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+
+  if (i === 0) dot.classList.add("active");
+
+  dot.addEventListener("click", () => {
+    index = i;
+    updateCarousel();
+    restartAutoSlide();
+  });
+
+  dotsContainer.appendChild(dot);
+});
+
+const dots = document.querySelectorAll(".dot");
+
+// atualizar carrossel
+function updateCarousel() {
+  track.style.transform = `translateX(-${index * 100}%)`;
+
+  dots.forEach(dot => dot.classList.remove("active"));
+  dots[index].classList.add("active");
+}
+
+// slide automático
+function slide() {
+  index++;
+
+  if (index >= cards.length) {
+    index = 0;
+  }
+
+  updateCarousel();
+}
+
+// autoplay
+function startSlide() {
+  autoSlide = setInterval(slide, 4000);
+}
+
+// reinicia ao clicar
+function restartAutoSlide() {
+  clearInterval(autoSlide);
+  startSlide();
+}
+
+// pause no hover
+track.addEventListener("mouseenter", () => clearInterval(autoSlide));
+track.addEventListener("mouseleave", startSlide);
+
+// iniciar
+startSlide();
